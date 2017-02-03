b2138c9e99133cdacc65edd5c41c8f9ea202ebe5
jonandrews <jcandrews@wpi.edu>
2017-02-03 18:41:17 -0500

    Add PatientController class stub

---
+++ src/main/java/PatientController.java
@@ -0,0 +1,36 @@
+/**
+ * Created by jonandrews on 2/3/17.
+ */
+
+public class PatientController {
+    PatientDisplay display;
+    GraphNode destination;
+    ApplicationController appController;
+
+    public PatientController(PatientDisplay theDis, GraphNode theDest, ApplicationController theAppCont) {
+        theDis = display;
+        theDest = destination;
+        theAppCont = appController;
+    }
+
+    public void search(String room) {
+        //return null;
+    }
+
+    public GraphNode select(String option) {
+        return null;
+    }
+
+    public void displayPath(GraphNodes[] path) {
+        //return null;
+    }
+
+    public void loginDirectoryAdmin(String login) {
+        //return null;
+    }
+
+    void update() {
+        //return null;
+    }
+
+}
