6d3e0143c290207109bfd19e11600d122019576c
Alex Tacescu <Alex Tacescu>
2017-02-03 19:13:38 -0500

    Add new classes
    ApplicationController, Room, GraphNode, MapAdminController, and
    DisplayController.

---
+++ src/main/java/MapAdminController.java
@@ -0,0 +1,38 @@
+/**
+ * Created by alext on 2/3/2017.
+ */
+public class MapAdminController {
+
+    MapAdminDisplay display;
+    GraphNode selectedNode;
+    GraphNode secondaryNode;
+
+    void update(){
+
+    }
+
+    public void login(String credentials){
+
+    }
+
+    public void addNode(FloorPoint location){
+
+    }
+
+    public GraphNode selectNode(FloorPoint loc){
+
+    }
+
+    public void deleteNode(GraphNode node){
+
+    }
+
+    public void addConnection(GraphNode nodeA, GraphNode nodeB){
+
+    }
+
+    public void deleteConnection(GraphNode nodeA, GraphNode nodeB){
+
+    }
+
+}
---
+++ src/main/java/Room.java
@@ -0,0 +1,24 @@
+/**
+ * Created by alext on 2/3/2017.
+ */
+public class Room {
+
+    GraphNode location;
+    String name;
+
+    public Room(GraphNode location, String name){
+
+    }
+
+    public setLocation(GraphNode loc){
+
+    }
+
+    public int compareTo(Room other){
+        return 0;
+    }
+
+
+
+
+}
