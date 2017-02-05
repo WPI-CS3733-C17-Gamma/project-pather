108b0591c1e3c0e42f1af40e2219df2eaa938904
Alex Tacescu <alextac98@gmail.com>
2017-02-05 14:30:26 -0500

    Add AddNode function, add Maps images

--- src/main/java/MapAdminController.java
+++ src/main/java/MapAdminController.java
@@ -1,8 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.*;
+import java.io.File;
+
 public class MapAdminController {
 
 //    MapAdminDisplay display;
     GraphNode selectedNode;
     GraphNode secondaryNode;
+
+    @FXML
+    private Button buttonSave;
+    @FXML
+    private Button buttonCancel;
+    @FXML
+    private ToggleButton togglebuttonAddNode;
+    @FXML
+    private ToggleButton togglebuttonAddConnections;
+    @FXML
+    private ImageView imageviewMap;
+
+    String mapName;
 
     void update(){
     }
@@ -11,6 +33,11 @@ public class MapAdminController {
     }
 
     public void addNode(FloorPoint location){
+        //need to add database save code
+    }
+
+    public void addNodeGraphically(){
+        addNode(getMouseLocation());
     }
 
     public GraphNode selectNode(FloorPoint loc){
@@ -24,6 +51,28 @@ public class MapAdminController {
     }
 
     public void deleteConnection(GraphNode nodeA, GraphNode nodeB){
+    }
+
+    private void setMap(String loc, String mapName){
+        File file = new File(loc);
+        Image map = new Image(file.toURI().toString());
+        imageviewMap.setImage(map);
+        this.mapName = mapName;
+    }
+
+    private void isClicked(){
+        if (togglebuttonAddNode.isSelected()){
+            addNodeGraphically();
+        } else if (togglebuttonAddConnections.isSelected()){
+            //add in AddConnections stuff
+        }
+    }
+
+    private FloorPoint getMouseLocation(){
+        PointerInfo a = MouseInfo.getPointerInfo();
+        Point b = a.getLocation();
+
+        return new FloorPoint(((int) b.getX()),(int) b.getY(),mapName);
     }
 
 }
