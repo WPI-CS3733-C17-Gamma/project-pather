2b199499493eb9c58195ec3d28ce5a6ea3e7a8d2
Dchang10 <dochang@wpi.edu>
2017-02-07 20:37:23 -0500

    Modify class
    PathNotFoundException: changed name to match naming conventions

--- src/main/java/PathNotFoundException.java
+++ src/main/java/PathNotFoundException.java
@@ -1,12 +1,12 @@
 /**
  * Created by Saina on 2017-02-07.
  */
-public class PATH_NOT_FOUND_EXCEPTION extends Exception{
+public class PathNotFoundException extends Exception{
 
     GraphNode nodeA;
     GraphNode nodeB;
 
-    PATH_NOT_FOUND_EXCEPTION(GraphNode nodeA, GraphNode nodeB){
+    PathNotFoundException(GraphNode nodeA, GraphNode nodeB){
         this.nodeA = nodeA;
         this.nodeB = nodeB;
     }
