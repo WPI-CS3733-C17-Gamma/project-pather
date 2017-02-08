d7d5b55887493982f67abcde5a007dbdee9cf25b
eula1998 <eula1998@gmail.com>
2017-02-08 17:38:43 -0500

    start adding high level test

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -7,15 +7,22 @@ import java.util.*;
 
 import static org.junit.Assert.*;
 
-public class DirectoryTest{
+/**
+ * Testing the search function from both the directory and the Patient Controller
+ */
+public class SearchTest{
     Directory d;
 
     Room albert = new Room(null, "3A");
     Room bernie = new Room(null, "4B");
     Room charles = new Room(null, "5C");
-    Room albert2 = new Room(null, "3A");
+    Room albert2 = new Room(null, "3B");
+    Room albert3 = new Room(null, "3A");
 
     static Scanner input = new Scanner(System.in);
+
+    PatientController p;
+    Map m;
 
     @Before
     public void setUp(){
@@ -33,6 +40,9 @@ public class DirectoryTest{
         d.addRoom(albert);
         d.addRoom(bernie);
         d.addRoom(charles);
+        d.addRoom(albert2);
+        m = new Map(d, null, null);
+        p = new PatientController(m, null, null);
     }
     @Test
     public void dummyTest(){
@@ -50,7 +60,7 @@ public class DirectoryTest{
         // System.err.println(result.toString()+"***********************************************");
         assertNotNull(result);
         assertListEquals(result, temp);
-        assertListEquals(d.searchRooms("3B"), new ArrayList());
+        assertListEquals(d.searchRooms("3C"), new ArrayList());
     }
 
     /**
@@ -58,7 +68,7 @@ public class DirectoryTest{
      */
     @Test
     public void compareToTest() {
-        assertEquals(albert.compareTo(albert2), 0);
+        assertEquals(albert.compareTo(albert3), 0);
         assertTrue(albert.compareTo(charles) < 0);
         assertTrue(charles.compareTo(albert) > 0);
     }
@@ -91,5 +101,32 @@ public class DirectoryTest{
         assertListEquals(d.searchEntries("Alb"), temp);
         temp.add("Bernie");
         assertListEquals(d.searchEntries("er"), temp);//return more than one results
+    }
+
+    @Test
+    public void searchEntryTestFromPC() {
+        List<String> temp = new ArrayList();
+        temp.add("Albert");
+        List result = p.search("Albert");
+        // System.err.println(result.toString()+"***********************************************");
+        assertListEquals(result, temp);
+        assertListEquals(p.search("Alb"), temp);
+        temp.add("Bernie");
+        assertListEquals(p.search("er"), temp);//return more than one results
+    }
+
+    @Test
+    public void CheckRoomTestFromPC(){
+        List<String> temp = new ArrayList<>();
+        temp.add("3A");
+        List result = p.search("3A");
+        assertListEquals(result, temp);
+        temp.clear();
+        temp.add("3B");
+        assertListEquals(p.search("3B"), temp);
+        temp.clear();
+        temp.add("3A");
+        temp.add("3B");
+        assertListEquals(p.search("3"), temp);
     }
 }
---
+++ src/test/java/SelectTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class SelectTest {
+
+    Map m;
+    PatientController p;
+
+    @Before
+    public void setUp(){
+        m = new Map(null, null, null);
+        p = new PatientController(null, null, null);
+    }
+}
