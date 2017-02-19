7a6e7225ec5e395e769784149cf5355921f23fc4
Dchang10 <dochang@wpi.edu>
2017-02-19 02:55:42 -0500

    Add class
    Made basic circular context menu class

--- src/main/java/CircularContextMenu.java
+++ src/main/java/CircularContextMenu.java
@@ -1,6 +1,8 @@
 import javafx.event.EventHandler;
-import javafx.scene.image.Image;
+import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -10,61 +12,118 @@ import java.util.List;
  * Created by dominic on 2/18/17.
  */
 public class CircularContextMenu {
-    List<ContextMenuElement> menuElements = new LinkedList<>();
+    int innerRadius;
+    int outerRadius;
+    private List<ContextMenuElement> menuElements = new LinkedList<>();
+    List<Node> drawnItems = new LinkedList<>();
 
     CircularContextMenu(){
+        this.innerRadius = 50;
+        this.outerRadius = 100;
         menuElements = new LinkedList<>();
     }
 
-    public void addOption(Image image, EventHandler handler){
+    CircularContextMenu(int innerRadius, int outerRadius){
+        this.innerRadius = innerRadius;
+        this.outerRadius = outerRadius;
+        menuElements = new LinkedList<>();
+    }
+
+    public void addOption(Paint image, EventHandler handler){
         int size = menuElements.size();
-            int angle = 360 / (size+1);
-        menuElements.add(new ContextMenuElement(angle, angle, image,50, 100));
+        double angle = 360 / (size+1);
+        double currentAngle = 0;
+
+        menuElements.add(new ContextMenuElement(image, handler));
+
+        for(ContextMenuElement element:menuElements) {//redraw elements when a new one is added
+            try{
+                element.draw(currentAngle, angle, this.innerRadius, this.outerRadius);
+            }catch(IllegalArgumentException e){
+                System.out.println("Angle parameters must be positive with angle > initialAngle.");
+            }
+            currentAngle = currentAngle + angle;
+        }
 
     }
-    public void addOption(Image image){
+    public void addOption(Paint image){//redraw elements when a new one is added
         int size = menuElements.size();
-        int angle = 360 / (size+1);
-        int currentAngle = 0;
-        menuElements.add(new ContextMenuElement(currentAngle, currentAngle, image, 50, 100));
+        double angle = 360 / (size+1);
+        double currentAngle = 0;
+
+        menuElements.add(new ContextMenuElement(image));
 
         for(ContextMenuElement element:menuElements) {
-            element.resize(currentAngle, currentAngle, 50, 100);
-            currentAngle += angle;
+            try{
+                element.draw(currentAngle, angle, this.innerRadius, this.outerRadius);
+            }catch(IllegalArgumentException e){
+                System.out.println("Angle parameters must be positive");
+            }
+
+            currentAngle = currentAngle + angle;
         }
 
     }
 
-    public void show(AnchorPane pane){
-        int numOptions = menuElements.size();
-        int angle = 360 / numOptions;
-        int currentAngle = 0;
-        int roatationAngle = 0;
+    /**
+     * Removes an option by index
+     * @param index
+     */
+    public void removeOption(int index){//redraw elements when a new one is added
+        int size = menuElements.size();
+        if(size > 0 && index < size) {
+            menuElements.remove(index);
+            double angle = 360 / (size - 1);
+            double currentAngle = 0;
 
-        double x;
-        double y;
-        for (ContextMenuElement element: menuElements) {
-            System.out.println("x: " +element.path.getLayoutX() + "y: " +element.path.getLayoutY());
 
-            roatationAngle += 360/(numOptions - 1);
+            for (ContextMenuElement element : menuElements) {
+                try {
+                    element.draw(currentAngle, angle, this.innerRadius, this.outerRadius);
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Angle parameters must be positive with angle > initialAngle.");
+                }
 
-//            element.path.setRotate(currentAngle);
-//            x =  element.outerRadius*Math.cos(currentAngle*Math.PI/360);
-//            y =  element.outerRadius*Math.sin(currentAngle*Math.PI/360);
-//            System.out.println("x: " + x + "y: " + y);
-            element.path.setLayoutX(300);
-            element.path.setLayoutY(300);
-//            currentAngle += angle;
-//            element.resize(angle, 150, 100);
-            pane.getChildren().add(element.path);
-            System.out.println("The angle is "+currentAngle);
-//            System.out.println("after x: " + x + "y: " + y);
+                currentAngle = currentAngle + angle;
+            }
         }
+    }
+
+    /**
+     * Get the number of options currently in menu
+     * @return
+     */
+    public int getNumOptions(){
+        return menuElements.size();
+    }
+    public void show(AnchorPane pane, int layoutX, int layoutY, int radius){
+        if( radius > 0){
+            Circle circ = new Circle(radius);
+            pane.getChildren().add(circ);
+            circ.setLayoutX(layoutX);
+            circ.setLayoutY(layoutY);
+            drawnItems.add(circ);
+        }
+        int i = 0;
+        for (ContextMenuElement element: menuElements) {
+            System.out.println(i++);
+            element.path.setLayoutX(layoutX);
+            element.path.setLayoutY(layoutY);
+            pane.getChildren().add(element.path);
+            drawnItems.add(element.path);
+        }
+    }
+
+    /**
+     * Getter for Menu Elements
+     * @return
+     */
+    public List<ContextMenuElement> getMenuElements(){
+        return menuElements = new LinkedList<>();
     }
 
     public String toString(){
-        System.out.println("Number of Elements" + menuElements.size());
-        return("Number of Elements" + menuElements.size());
+        return("This menu has: " + menuElements.size() + " options.");
     }
 }
 
--- src/main/java/ContextMenuElement.java
+++ src/main/java/ContextMenuElement.java
@@ -1,75 +1,95 @@
 import javafx.event.EventHandler;
-import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.paint.ImagePattern;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.SVGPath;
 
 /**
  * Created by dominic on 2/18/17.
  */
 public class ContextMenuElement{
-    Image icon;
+    Paint icon;
     SVGPath path = new SVGPath();
     int outerRadius = 150;
     int innerRadius = 100;
     double angle = 10;
     double initialAngle = 10;
+    CircularContextMenu parentMenu = new CircularContextMenu();
 
 
-    EventHandler<MouseEvent> onClickHandler = new EventHandler<MouseEvent>() {
+
+    EventHandler<MouseEvent> onClickHandler = new EventHandler<MouseEvent>() { //Default Event Handler
         @Override
         public void handle(MouseEvent event) {
-            System.out.println("Hi");
+            System.out.println("This button has no Event");
         }
     };
-    ContextMenuElement(double initialAngle, double angle, Image icon, int innerRadius, int outerRadius){
-        this.path.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickHandler);
-        this.innerRadius = innerRadius;
-        this.outerRadius = outerRadius;
-        this.icon = icon;
-        this.initialAngle = initialAngle*Math.PI/180;
-        this.angle = angle*Math.PI/180;
 
-        int difference = outerRadius - innerRadius;
-        System.out.println("Initial angle "+ initialAngle + "Outer angle " + outerRadius);
-        path.setContent("M0,0 m" + innerRadius*Math.sin(-angle + initialAngle) + ","+ -innerRadius*Math.cos(-angle + initialAngle) +", l"+
-            difference*Math.sin(-angle + initialAngle)+","+ -difference*Math.cos(-angle + initialAngle)+" a"+outerRadius+","+outerRadius+" 0 0,0" +
-            -outerRadius*(Math.sin(initialAngle) )+","+ outerRadius*(1- Math.cos(initialAngle) ) +" l" + difference*Math.sin(initialAngle)+"," +
-            difference*Math.cos(initialAngle)+ "z");
-        path.setFill(new ImagePattern(this.icon, 20, 20, 40, 40, false));
+    /**
+     * Constructor for a button that does not do anything
+     * @param icon
+     */
+    ContextMenuElement( Paint icon){
+        this.path.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickHandler);
+        this.icon = icon;
+
     }
 
-    ContextMenuElement(double initialAngle, double angle, Image icon, EventHandler onClickHandler, int innerRadius, int outerRadius){
+    /**
+     * Constructor
+     * @param icon
+     * @param onClickHandler
+     */
+    ContextMenuElement( Paint icon, EventHandler onClickHandler){
         this.onClickHandler = onClickHandler;
         this.path.addEventHandler(MouseEvent.MOUSE_CLICKED, onClickHandler);
-        this.innerRadius = innerRadius;
-        this.outerRadius = outerRadius;
         this.icon = icon;
-        this.initialAngle = initialAngle*Math.PI/180;
-        this.angle = angle*Math.PI/180;
-
-        int difference = outerRadius - innerRadius;
-        angle = angle*Math.PI/180;
-        path.setContent("M0,0 m 0, -"+ innerRadius +", v-"+difference+" a"+outerRadius+","+outerRadius+" 0 0,0" +
-            -outerRadius*(Math.sin(angle) )+","+ outerRadius*(1- Math.cos(angle) ) +" l" + difference*Math.sin(angle)+"," +
-            difference*Math.cos(angle)+ "z");
-        path.setFill(new ImagePattern(this.icon, 20, 20, 40, 40, false));
     }
 
-
-    public void resize(double initialAngle, double angle, int innerRadius, int outerRadius){
+    /**
+     * Draws contextmenu element starting at an initial angle, arcing through angle degrees
+     * @param initialAngle Angle to start drawing from
+     * @param angle Angle swept through
+     * @param innerRadius inner radius size
+     * @param outerRadius Size of element
+     */
+    public void draw(double initialAngle, double angle, int innerRadius, int outerRadius) throws IllegalArgumentException{
         this.innerRadius = innerRadius;
         this.outerRadius = outerRadius;
-
         int difference = outerRadius - innerRadius;
+
+        if((angle  < 0) || initialAngle < 0 ){
+            throw new IllegalArgumentException();
+        }
+
         this.angle = angle*Math.PI/180;
         this. initialAngle = initialAngle*Math.PI/180;
-        System.out.println("Initial angle "+ initialAngle + "Outer angle " + outerRadius);
 
-        path.setContent("M0,0 m 0, -"+ innerRadius +", v-"+difference+" a"+outerRadius+","+outerRadius+" 0 0,0" +
-            -outerRadius*(Math.sin(angle) )+","+ outerRadius*(1- Math.cos(angle) ) +" l" + difference*Math.cos(angle)+"," +
-            difference*Math.sin(angle)+ "z");
-        path.setFill(new ImagePattern(this.icon, 20, 20, 40, 40, false));
+        double point1x = innerRadius*Math.cos(this.initialAngle);
+        double point1y = innerRadius*Math.sin(this.initialAngle);
+
+        double point2x = difference*Math.cos(this.initialAngle);
+        double point2y = difference*Math.sin(this.initialAngle);
+
+        double point3x = outerRadius*Math.cos(this.angle + this.initialAngle) - point2x - point1x;
+        double point3y = outerRadius*Math.sin(this.angle + this.initialAngle) - point2y - point1y;
+
+        double point4x = -difference*Math.cos(this.angle + this.initialAngle);
+        double point4y = -difference*Math.sin(this.angle + this.initialAngle);
+
+
+        if((this.angle - this.initialAngle) <= 180) {
+            path.setContent("M0,0 m" + point1x + "," + point1y +
+                " l " + point2x + "," + point2y + "a" + outerRadius + "," +
+                outerRadius + " 0 0,1 " + point3x + "," + point3y + "l " + point4x + "," + point4y + " z");
+        }
+        else {
+            path.setContent("M0,0 m" + point1x + "," + point1y +
+                " l " + point2x + "," + point2y + "a" + outerRadius + "," +
+                outerRadius + " 0 1,1 " + point3x + "," + point3y + " l " + point4x + "," + point4y + " z");
+        }
+
+
+        path.setFill(icon);
     }
 
 
