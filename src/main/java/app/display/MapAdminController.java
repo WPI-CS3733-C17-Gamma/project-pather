1b0ffd631c299a300d60b8a491fbf8f5c14e3d53
Alex Tacescu <alextac98@gmail.com>
2017-02-22 21:54:37 -0500

    add key backspace to delete connections

--- src/main/java/app/display/MapAdminController.java
+++ src/main/java/app/display/MapAdminController.java
@@ -713,9 +713,14 @@ public class MapAdminController extends DisplayController {
     @FXML
     public void handleKey(KeyEvent key){
         if (!roomName.isFocused()) {
+            System.out.println("hi mom!");
             switch (key.getCode()) {
                 case DELETE:
                     deleteSelected();
+                    break;
+                case BACK_SPACE:
+                    System.out.println("delete");
+                    deleteConnection();
                     break;
                 case N:
                     changeState(State.ADD_NODES);
@@ -730,7 +735,10 @@ public class MapAdminController extends DisplayController {
                     changeState(State.CHAIN_ADD);
                     break;
             }
+        }else {
+            System.out.println("Hi mom");
         }
+        System.out.println("Hi mom!");
     }
 
     /**
