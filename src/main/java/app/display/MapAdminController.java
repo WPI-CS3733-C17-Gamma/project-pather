8f23f043329b22dc8300bf726258398d1f7540a0
Dchang10 <dochang@wpi.edu>
2017-02-28 23:37:04 -0500

    Fix Bug
    Fixed null pointer exception bug in selectNode

--- src/main/java/app/display/MapAdminController.java
+++ src/main/java/app/display/MapAdminController.java
@@ -933,8 +933,9 @@ public class MapAdminController extends DisplayController {
         buttonDeleteNode.setDisable(false);
         roomName.setDisable(false);
         buttonAddRoom.setDisable(false);
+
         buttonDeleteConnection.setDisable(
-            secondaryNode == null ||
+            secondaryNode == null || selectedNode == null ||
             !selectedNode.getAdjacent().contains(secondaryNode));
         buttonDeleteElevator.setDisable(selectedNode == null ||
                                         !selectedNode.doesCrossFloor());
@@ -1244,6 +1245,7 @@ public class MapAdminController extends DisplayController {
     public void showContextMenu(ContextMenuEvent event){
         contextEvent = event;
         selectNode(nearbyNodeContext(contextEvent));
+
 
 //        ContextMenu contextMenu = new ContextMenu();
 //
